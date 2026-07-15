ee247040fa347573a96ca0089ce04ae1f1a66aa8
dk004 <arpitdk123@gmail.com>
2026-07-15 22:51:24 +0530

    Data types in angular using TypeScript

--- src/app/app.ts
+++ src/app/app.ts
@@ -9,4 +9,32 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('AngularFullCourse');
+
+  // this will throw error at the complie time./runtime also it will show.
+  // value=10;
+  // value = "Arpit";
+
+// defining with type
+ name: string = 'Arpit';
+ age :number =55;
+ isValid:boolean = true;
+ 
+ something:any = "Something"; //can store any datatype in any (not recommended)
+ notSure : unknown= 18; //same as any but less danger than it
+
+
+//  multiple values can be given to property (class variables)
+StringOrNumber: string | number = 98; //union types
+
+
+// error will not be tracked
+handleClick(){
+    let value;  //first needs to be declare
+    value= 10;  //first initialization
+    value ="Arpit Alkari";  //second initialization (this will be considered)
+    console.log(value);
+    return value;
+  }
+  display:string = this.handleClick();
+
 }
