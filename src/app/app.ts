217dd1a54db8374714729ec83a0cf4c893aa7bd2
dk004 <arpitdk123@gmail.com>
2026-07-15 18:06:18 +0530

    This is Angular for creating custom components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SigninComponent } from './signin/signin';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SigninComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
