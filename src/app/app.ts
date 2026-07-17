f943f992cf0d45a3631bd8b9029085c7597c6109
dk004 <arpitdk123@gmail.com>
2026-07-17 18:58:57 +0530

    Simple Counter App : project

--- src/app/app.ts
+++ src/app/app.ts
@@ -9,4 +9,38 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('AngularFullCourse');
+
+  counter : number = 0;
+
+  // handleAdd(){
+  //   this.counter++;
+  // }
+
+  // handleMinus(){
+  //   if(this.counter>0){
+  //     this.counter--;
+  //   }
+  // }
+
+  // handleReset(){
+  //   this.counter = 0;
+  // }
+
+
+
+  handle(value : string){
+
+    if(value === 'plus'){
+      this.counter++;
+    }else if(value === 'minus'){
+      if(this.counter > 0){
+        this.counter--;
+      }
+    }else{
+      this.counter = 0;
+    }
+
+  }
+
+
 }
