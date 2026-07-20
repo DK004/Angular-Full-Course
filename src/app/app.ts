0bc8d8c65856560a2030d453b6f5762cff75c124
dk004 <arpitdk123@gmail.com>
2026-07-20 19:06:35 +0530

    two way bindings

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,15 @@
 import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('AngularFullCourse');
+  userName = "";
+  fullName = signal("");
 }
