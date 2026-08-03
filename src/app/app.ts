5743451c53a7f224f814d427dba539ade865af76
dk004 <arpitdk123@gmail.com>
2026-08-03 22:22:32 +0530

    Reusable components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,32 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Card } from './card/card';
+
+interface cards{
+  title : string,
+  description :string 
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Card],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('AngularFullCourse');
+
+cards1 = signal<cards[]>([{
+  title : "Card 1",
+  description : "This is the desc for card 1"
+},
+{
+  title : "Card 2",
+  description : "This is the desc for card 2"
+},
+{
+  title : "Card 3",
+  description : "This is the desc for card 3"
+}]);
+
 }
