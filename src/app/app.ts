92f366bf3e88d21c3635d3d1e46b714d491c5620
dk004 <arpitdk123@gmail.com>
2026-08-03 21:44:38 +0530

    Angular Forms

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,25 @@
 import { Component, signal } from '@angular/core';
+import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, ReactiveFormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('AngularFullCourse');
+  name = signal("");
+  email = signal("");
+
+  submitForm(){
+    console.log(`Name : ${this.name} , Email : ${this.email}`);
+  }
+
+  formToggle = true;
+  toogleForm(){
+    this.formToggle = !this.formToggle;
+  }
+
 }
