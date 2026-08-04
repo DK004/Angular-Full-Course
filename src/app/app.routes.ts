2c7f41376d4606e632b739a226718f1290f64f40
dk004 <arpitdk123@gmail.com>
2026-08-04 19:30:24 +0530

    child/nested Routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { Dashboard } from './dashboard/dashboard';
+import { Profile } from './dashboard/profile/profile';
+import { Settings } from './dashboard/settings/settings';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path : 'dashboard',
+        component : Dashboard,
+        children : [
+            {path : "profile" , component : Profile},
+            {path : "settings" , component : Settings}
+        ]
+    }
+    ,
+    {path : "", redirectTo : 'dashboard', pathMatch : 'full'}
+    
+];
